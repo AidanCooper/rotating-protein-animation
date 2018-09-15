2e0bceba09dae23b68f3743aac6e6a141e44b796
Aidan Cooper <aidantcooper@gmail.com>
2018-09-16 00:01:08 +0100

    Amend convert.py
    PEP8 standards

--- animations/convert.py
+++ animations/convert.py
@@ -1,13 +1,14 @@
 import imageio
 import os, sys
 
+
 class TargetFormat(object):
     GIF = ".gif"
     MP4 = ".mp4"
     AVI = ".avi"
 
+    
 def convertFile(inputpath, targetFormat):
-    """Reference: http://imageio.readthedocs.io/en/latest/examples.html#convert-a-movie"""
     outputpath = os.path.splitext(inputpath)[0] + targetFormat
     print("converting\r\n\t{0}\r\nto\r\n\t{1}".format(inputpath, outputpath))
 
@@ -19,9 +20,10 @@ def convertFile(inputpath, targetFormat):
         sys.stdout.write("\rframe {0}".format(i))
         sys.stdout.flush()
         writer.append_data(im)
-    print("\r\nFinalizing...")
+    print("\r\nFinalising...")
     writer.close()
     print("Done.")
 
+    
 convertFile("constant_rotation_100quality.avi", TargetFormat.GIF)
 convertFile("default_rotation_90quality.avi", TargetFormat.GIF)
